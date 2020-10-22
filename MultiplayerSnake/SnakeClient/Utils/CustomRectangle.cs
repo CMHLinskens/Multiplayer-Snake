89f28bb24fefb025944f785034f98d454cca216b
Bart van Moorsel <bartvanmoorsel7@gmail.com>
2020-10-22 21:38:06 +0200

    Created snake image

---
+++ MultiplayerSnake/SnakeClient/Utils/CustomRectangle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Media;
+
+namespace SnakeClient.Utils
+{
+    class CustomRectangle : CustomObservableObject
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public double CanvasWidth { get; set; }
+        public double CanvasHeight { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Brush Color { get; set; }
+        public CustomRectangle(int x, int y, int width, int height, double canvasWidth, double canvasHeight, int value)
+        {
+            Width = width;
+            Height = height;
+            CanvasWidth = canvasWidth;
+            CanvasHeight = canvasHeight;
+            X = x;
+            Y = y;
+
+            Color = value switch
+            {
+                1 => Brushes.Red,
+                2 => Brushes.Green,
+                3 => Brushes.Blue,
+                4 => Brushes.Yellow,
+                5 => Brushes.Orange,
+                _ => Brushes.White,
+            };
+        }
+    }
+}
--- MultiplayerSnake/SnakeClient/ViewModels/SnakeViewModel.cs
+++ MultiplayerSnake/SnakeClient/ViewModels/SnakeViewModel.cs
@@ -1,10 +1,14 @@
 ﻿using SnakeClient.Utils;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
 using System.Text;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Ink;
 using System.Windows.Input;
+using System.Windows.Media;
 using Utils;
 
 namespace SnakeClient.ViewModels
@@ -13,21 +17,59 @@ namespace SnakeClient.ViewModels
     {
         private int[,] gameField;
         private Direction currentDirection;
+        private DrawingContext drawingContext;
+        public double Height { get; set; } = 920;
+        public double Width { get; set; } = 1140;
         public ICommand KeyDownCommand{ get; set; }
         public ICommand KeyLeftCommand { get; set; }
         public ICommand KeyUpCommand { get; set; }
         public ICommand KeyRightCommand { get; set; }
         public StrokeCollection StrokeCollection { get; set; }
+        public ObservableCollection<CustomRectangle> Rectangles{ get; set; }
 
         public SnakeViewModel(ShellViewModel shellViewModel)
         {
             StrokeCollection = new StrokeCollection();
-            
+            this.drawingContext = new DrawingVisual().RenderOpen();
+            Rectangles = new ObservableCollection<CustomRectangle>();
+
+            //Test
+            gameField = new int[16, 16];
+            for (int y = 0; y < 16; y++)
+            {
+                for (int x = 0; x < 16; x++)
+                {
+                    gameField[y, x] = 0;
+                }
+            }
+
+            gameField[3, 3] = 2;
+            gameField[3, 4] = 2;
+            gameField[3, 5] = 2;
+            gameField[7, 6] = 3;
+            gameField[8, 6] = 3;
+            gameField[9, 6] = 3;
+
+            Draw();
         }
 
+        /*
+         * Creates the rectangles and adds them to the Rectangles collection. "Draws" the 2d array.
+         */
         private void Draw()
         {
-            //StrokeCollection.Draw()
+            double squareRootFieldLength  = Math.Sqrt(gameField.Length);
+            for (int y = 0; y < squareRootFieldLength; y++)
+            {
+                for (int x = 0; x < squareRootFieldLength; x++)
+                {
+                    if (gameField[y, x] > 0)
+                    {
+                        // Add a rectangle to the collection
+                        Rectangles.Add(new CustomRectangle((int)((x / squareRootFieldLength) * Width), (int)((y / squareRootFieldLength) * Height), (int)(Width / squareRootFieldLength), (int)(Height / squareRootFieldLength), Width, Height, gameField[y, x]));
+                    }
+                }
+            }
         }
     }
 }
